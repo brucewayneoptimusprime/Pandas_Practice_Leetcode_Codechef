f2161c6f7662f5cf2ee717d1f0153da8271e4f7e
ADITYA VIKRAM SINGH <adityapersonalusefighttry@gmail.com>
2026-08-04 19:02:54 +0530

    Update Dtype_checking_pandas_specialized_library.py

--- Dtype_checking_pandas_specialized_library.py
+++ Dtype_checking_pandas_specialized_library.py
@@ -1,4 +1,13 @@
-from pandas.api.types import is_numeric_dtype
+from pandas.api.types import (
+    is_numeric_dtype,
+    is_integer_dtype,
+    is_float_dtype,
+    is_bool_dtype,
+    is_string_dtype,
+    is_object_dtype,
+    is_datetime64_any_dtype,
+    is_timedelta64_dtype
+)
 
 for column in df.columns:
 
